f73951e88b6544cf5697661a62fd657857082f5d
Jonathan Johnson <me@jondavidjohn.com>
2018-10-20 22:07:59 -0500

    some re-org

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,6 @@ set -eu
 : "${WEBPACK_CONFIG_PATH:?'You must provide the path to your webpack configuration with the WEBPACK_CONFIG environment variable'}"
 : "${PT_PROJECT_TOKEN:?'You must set the PT_PROJECT_TOKEN secret'}"
 
-event_file='/github/workflow/event.json'
-export PT_BRANCH=$(echo $GITHUB_REF | sed "s/refs\/heads\///g")
-export PT_AUTHOR=$(cat $event_file | jq -r '.head_commit.author.email')
-export PT_MESSAGE=$(cat $event_file | jq -r '.head_commit.message')
-export PT_COMMIT=$GITHUB_SHA
-export PT_COMMITTED_AT=$(date -d $(cat $event_file | jq -r '.head_commit.timestamp') +'%s')
-export PT_PRIOR_COMMIT=$(cat $event_file | jq -r '.before')
-
 if [ -e yarn.lock ]; then
   if ! yarn install; then
     echo "yarn install failed" 1>&2
@@ -28,4 +20,4 @@ else
   npm install @packtracker/webpack-plugin@1.0.1
 fi
 
-PT_UPLOAD=true node ./webpack.js
+node ./report.js
