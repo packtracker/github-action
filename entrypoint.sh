bd400fa1c746a21a39523dd94d75975c592acca6
Jonathan Johnson <me@jondavidjohn.com>
2019-03-02 20:48:07 -0600

    bump beta tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,14 +10,14 @@ if [ -e yarn.lock ]; then
     exit 1
   fi
 
-  yarn add @packtracker/webpack-plugin@2.0.0-beta.14
+  yarn add @packtracker/webpack-plugin@2.0.0-beta.15
 else
   if ! npm install; then
     echo "npm install failed" 1>&2
     exit 1
   fi
 
-  npm install @packtracker/webpack-plugin@2.0.0-beta.14
+  npm install @packtracker/webpack-plugin@2.0.0-beta.15
 fi
 
 cp /report.js ./report.js
