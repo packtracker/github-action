e6c9d767f49a0809a80c3d50bbe25e113ed5c21a
Jonathan Johnson <me@jondavidjohn.com>
2019-05-13 16:04:02 -0500

    add PROJECT_ROOT option to configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ set -eu
 
 : "${PT_PROJECT_TOKEN:?'You must set the PT_PROJECT_TOKEN secret'}"
 
+if [ -z "$PT_PROJECT_ROOT" ]; then
+  cd $PT_PROJECT_ROOT
+fi
+
 if [ -e yarn.lock ]; then
   if ! yarn install; then
     echo "yarn install failed" 1>&2
@@ -11,13 +15,15 @@ if [ -e yarn.lock ]; then
   fi
 
   yarn add @packtracker/webpack-plugin@2.0.1
-else
+elif [ -e package.json ]; then
   if ! npm install; then
     echo "npm install failed" 1>&2
     exit 1
   fi
 
   npm install @packtracker/webpack-plugin@2.0.1
+else
+  echo "Could not find package.json file in $(pwd)"
 fi
 
 cp /report.js ./report.js
